f5cbddb759534afe99d56beb35724ccbe21f8e61
philsiff <filman74@verizon.net>
2014-08-16 17:32:52 -0400

    Added movement to green square for testing(Arrows)

--- js/target.js
+++ js/target.js
@@ -1,6 +1,23 @@
 Target = function(x, y){
 	this.x = x;
 	this.y = y;
+	this.speed = 500;
+};
+
+Target.prototype.update = function(delta) {
+    if (37 in keysDown) { //left
+        this.x -= this.speed * delta;
+    }
+    if (38 in keysDown) { //up
+        this.y -= this.speed * delta;
+    }
+    if (39 in keysDown) { //right
+        this.x += this.speed * delta;
+    }
+    if (40 in keysDown) { //down
+        this.y += this.speed * delta;
+    }
+
 };
 
 Target.prototype.render = function() {
